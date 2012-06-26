93ab282878517af62942e6b7349853129c26625b
Kohe Tokoi <tokoik@gmail.com>
2012-06-26 23:50:10 +0900

    update dependency

---
+++ texture23.d
@@ -0,0 +1,6 @@
+main1.o: main1.cpp trackball.h scene.h
+main2.o: main2.cpp trackball.h scene.h
+main3.o: main3.cpp trackball.h scene.h
+saveimage.o: saveimage.cpp
+scene.o: scene.cpp scene.h
+trackball.o: trackball.cpp trackball.h
